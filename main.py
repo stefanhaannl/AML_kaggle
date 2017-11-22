aca99166f3f99224df457882e6e9ba61538cb14a
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-11-22 21:49:04 +0100

    load_data function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 Main working directory
 """
+import preprocessing
+import numpy as np
+import pandas as pd
+
+def load_data(n=0,train=True):
+    df = preprocessing.load_images(n,train)
+    if train == True:
+        df = preprocessing.add_labels(df)
+    return df
