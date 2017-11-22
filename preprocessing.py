41c6d8ed10e92fca0b68a24c09c5dcf78041b054
Rick Bruins <rickbruins020@gmail.com>
2017-11-22 16:09:11 +0100

    working load_images func

--- preprocessing.py
+++ preprocessing.py
@@ -4,14 +4,38 @@ Preprocessing file for loading the information in a workable format.
 import numpy as np
 import pandas as pd
 import os
+from PIL import Image
 
-WORKING_PATH = os.path.abspath()
+WORKING_PATH = os.path.abspath('')
 DATA_PATH = os.path.join(WORKING_PATH,'data')
 
-def load_images(n=0, train=True):
+def load_images(n=0,train=True):
     """
     Import either test or training images into a pd.dataframe object of np.arrays (images) and label numbers
     """
+    im_list = []
+    label_list = []
+    
+    path = ""
+    if train == True:
+        path = "/train/"
+    else:
+        path = "/test/"
+    
+    for filename in os.listdir(DATA_PATH+path):
+        x = np.array([np.array(Image.open(DATA_PATH+path+filename))])
+        im_list.append(x) 
+        id_im = int(filename.split('.')[0])
+        label_list.append(id_im)
+        n-=1
+        if n == 0:
+            break
+                
+    data_dict = {"image":im_list,"number":label_list}
+    df = pd.DataFrame(data_dict)
+    print df
+        
+        
     pass
 
 def add_labels(images, train=True):
