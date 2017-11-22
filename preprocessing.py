66d56b5dd79f038ae5c1dd310b502822972711b4
stefanhaannl <31733054+stefanhaannl@users.noreply.github.com>
2017-11-22 14:45:28 +0100

    template

--- preprocessing.py
+++ preprocessing.py
@@ -3,11 +3,14 @@ Preprocessing file for loading the information in a workable format.
 """
 import numpy as np
 import pandas as pd
+import os
 
+WORKING_PATH = os.path.abspath()
+DATA_PATH = os.path.join(WORKING_PATH,'data')
 
-def load_images(n=-1, train=True):
+def load_images(n=0, train=True):
     """
-    Import either test or training images into a pd.series object of np.arrays (images)
+    Import either test or training images into a pd.dataframe object of np.arrays (images) and label numbers
     """
     pass
 
